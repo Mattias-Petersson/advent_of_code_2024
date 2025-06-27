f84469373c58afa1789a2b94d4c7a25abeea0bf5
Mattias Petersson <Mattias.Petersson93@gmail.com>
2025-06-27 21:23:18 +0200

    Day 4 Part 1 done

--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -11,10 +11,10 @@ const _MATCH_PHRASES: [[char; 4]; 2] = [['X', 'M', 'A', 'S'], ['S', 'A', 'M', 'X
 pub fn exercise() {
     let result = || -> Result<usize, Box<dyn Error>> {
         let _input = get_input()?;
-        let hori = read_horizontal(&_input);
-        println!("{hori}");
-        let vert = read_vertical(&_input);
-        Ok(hori + vert)
+        let horizontal_count = read_horizontal(&_input);
+        let vertical_count = read_vertical(&_input);
+        let diagonal_count = read_diagonal(&_input);
+        Ok(horizontal_count + vertical_count + diagonal_count)
     };
     match result() {
         Ok(val) => println!("Occurrences of \"XMAS\" & \"SAMX\" in input: {val}"),
@@ -56,5 +56,49 @@ fn read_vertical(input: &Vec<Vec<char>>) -> usize {
 }
 
 fn read_diagonal(input: &Vec<Vec<char>>) -> usize {
-    todo!();
+    let mut diagonals: Vec<Vec<char>> = Vec::new();
+    acc_diagonals(input, &mut diagonals);
+    count_matches(&diagonals)
+}
+
+fn acc_diagonals(input: &Vec<Vec<char>>, acc_diags: &mut Vec<Vec<char>>) {
+    for col_start in 0..input[0].len() {
+        let left_to_right = get_one_diagonal(input, 0, col_start, 1);
+        let right_to_left = get_one_diagonal(input, 0, col_start, -1);
+
+        acc_diags.push(left_to_right);
+        acc_diags.push(right_to_left);
+    }
+    for row_start in 1..input.len() {
+        let left_to_right = get_one_diagonal(input, row_start, 0, 1);
+        let right_to_left = get_one_diagonal(input, row_start, input[0].len() - 1, -1);
+
+        acc_diags.push(left_to_right);
+        acc_diags.push(right_to_left);
+    }
+}
+
+/// col_delta is positive when going left-to-right, and negative when going right-to-left.
+fn get_one_diagonal(
+    input: &Vec<Vec<char>>,
+    row_start: usize,
+    col_start: usize,
+    col_delta: isize,
+) -> Vec<char> {
+    let num_rows = input.len();
+    let num_cols = input[0].len();
+    let mut diag: Vec<_> = Vec::new();
+
+    let mut curr_row = row_start;
+    let mut curr_col = col_start as isize;
+
+    while curr_row < num_rows && (curr_col as usize) < num_cols {
+        diag.push(input[curr_row][curr_col as usize]);
+        curr_row += 1;
+        if curr_col == 0 && col_delta < 0 {
+            break;
+        }
+        curr_col += col_delta;
+    }
+    diag
 }

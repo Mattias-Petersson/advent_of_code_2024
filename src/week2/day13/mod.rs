2776a2d2042ed742ee3e32c4fdcf7940b707ca1f
Mattias Petersson <Mattias.Petersson93@gmail.com>
2025-08-28 21:46:03 +0200

    Day 13 part 2 done

--- src/week2/day13/mod.rs
+++ src/week2/day13/mod.rs
@@ -1,20 +1,32 @@
-use std::fs;
+use std::{fs, ops::Add};
 
 struct Pair {
-    x: i32,
-    y: i32,
+    x: i64,
+    y: i64,
 }
 
 impl Pair {
-    pub fn new(x: i32, y: i32) -> Self {
+    pub fn new(x: i64, y: i64) -> Self {
         Self { x, y }
     }
 }
+
+impl Add<i64> for Pair {
+    type Output = Pair;
+
+    fn add(self, rhs: i64) -> Pair {
+        Pair {
+            x: self.x + rhs,
+            y: self.y + rhs,
+        }
+    }
+}
+
 fn parse_pair(line: &str) -> Pair {
-    let nums: Vec<i32> = line
+    let nums: Vec<i64> = line
         .split(|c: char| !c.is_ascii_digit())
         .filter(|s| !s.is_empty())
-        .map(|s| s.parse::<i32>().unwrap())
+        .map(|s| s.parse::<i64>().unwrap())
         .collect();
 
     Pair::new(nums[0], nums[1])
@@ -22,6 +34,7 @@ fn parse_pair(line: &str) -> Pair {
 
 fn read_input() -> Vec<(Pair, Pair, Pair)> {
     let mut res = Vec::new();
+    let constant_factor = 10000000000000;
     let input = fs::read_to_string("src/week2/day13/input.txt").unwrap();
     let mut lines = input.lines().filter(|l| !l.trim().is_empty());
     while let (Some(first_line), Some(second_line), Some(prize_line)) =
@@ -29,7 +42,8 @@ fn read_input() -> Vec<(Pair, Pair, Pair)> {
     {
         let first_equation = parse_pair(first_line);
         let second_equation = parse_pair(second_line);
-        let ans = parse_pair(prize_line);
+        let mut ans = parse_pair(prize_line);
+        ans = ans + constant_factor; // For part 2
         res.push((first_equation, second_equation, ans));
     }
     res
@@ -38,7 +52,6 @@ fn read_input() -> Vec<(Pair, Pair, Pair)> {
 pub fn exercise() {
     let mut count = 0;
     let problems = read_input();
-    // println!("{}", x1);
     for i in problems {
         if let Some(solved) = cramers_rule(i) {
             count += solved.x * 3 + solved.y;
@@ -53,7 +66,7 @@ fn cramers_rule((first, second, res): (Pair, Pair, Pair)) -> Option<Pair> {
     let numerator_y = res.y * first.x - res.x * first.y;
 
     if numerator_x % determinant != 0 || numerator_y % determinant != 0 {
-        // Either no solution or infinite, not relevant to the problem.
+        // Either no solution or infinite, either way not relevant to the problem.
         return None;
     }
 
